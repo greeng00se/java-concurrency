fed67d6406bdbc14dcdaee090c58a89ff53a06e2
greeng00se <thegreengoose7@gmail.com>
2022-09-24 23:33:17 +0900

    synchronized & transactional

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -4,6 +4,7 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StockService {
@@ -14,7 +15,15 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    public void decrease(Long id, Long quantity) {
+    /**
+     * @Transactional 사용시 래핑한 클래스를 새로 만들어서 실행
+     * decrease 메서드의 전 후로 트랜잭션 시작과 종료를 하는데 종료 시점에
+     * decrease 메서드를 다른 스레드가 호출할 수 있음
+     * @param id
+     * @param quantity
+     */
+//    @Transactional
+    public synchronized void decrease(Long id, Long quantity) {
         Stock stock = stockRepository.findById(id).orElseThrow();
 
         stock.decrease(quantity);
--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -5,9 +5,14 @@ import com.example.stock.repository.StockRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,25 +27,48 @@ class StockServiceTest {
     private StockRepository stockRepository;
 
     @BeforeEach
-    public void before() {
+    void before() {
         Stock stock = new Stock(1L, 100L);
 
         stockRepository.saveAndFlush(stock);
     }
 
     @AfterEach
-    public void after() {
+    void after() {
         stockRepository.deleteAll();
     }
 
     @Test
-    public void stock_decrease() {
+    void stock_decrease() {
         stockService.decrease(1L, 1L);
 
         // 100 - 1 = 99
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
         assertThat(stock.getQuantity()).isEqualTo(99);
+    }
+
+    @Test
+    void 동시에_100개_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService service = Executors.newFixedThreadPool(8);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            service.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        assertThat(stock.getQuantity()).isEqualTo(0);
     }
 
 }
